dc1f9580413009bce6172e75d4eb1e4068f2216d
Kei Sawada <k@swd.cc>
2018-07-15 01:38:29 +0900

    Add another boundary for sentinel handling to make testInitCannotBeCalledMoreThanOnce work

--- core/ruby2.5Action/rackapp/middleware/sentinel_handler.rb
+++ core/ruby2.5Action/rackapp/middleware/sentinel_handler.rb
@@ -3,7 +3,7 @@ require "#{__dir__}/base.rb"
 class SentinelHandler < MiddlewareBase
   def call(env)
     response = @app.call(env)
-    if !(env['REQUEST_PATH'] == '/init' && response.status == 200) then
+    if !(env['REQUEST_PATH'] == '/init' && [200,403].include?(response.status)) then
       puts response.body if response.status!=200
       puts "XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX"
       warn "XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX"
