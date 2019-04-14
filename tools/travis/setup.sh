dd9a15820a1803de7ed0d215a643be1de08a622b
Michele Sciabarra <msciabarra@apache.org>
2019-04-14 13:00:07 +0200

    installing the right docker for trusty

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -19,11 +19,13 @@
 set -e
 
 # upgrade docker
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" > /etc/apt/sources.list.d/docker.list'
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-get update
-sudo apt-key update
-sudo apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install docker-engine=1.11.1-0~precise
+sudo apt-get -y update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+sudo apt-get remove docker docker-engine
+apt-get -y install docker-ce=17.06.2~ce-0~ubuntu
 
 # Build script for Travis-CI.
 
