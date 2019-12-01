b82f89f5cdd591b707302570a8ad4b5235a49d8c
David Grove <dgrove-oss@users.noreply.github.com>
2019-12-01 15:18:27 -0500

    travis - dist:xenial and language:java (#37)
    * travis - dist:xenial and language:java
    
    * xenial has good enough version of docker; don't need to do anything in setup
    
    * get docker-ce addon

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -18,15 +18,6 @@
 
 set -e
 
-# upgrade docker
-sudo apt-get -y update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get remove docker docker-engine
-sudo apt-get -y install docker-ce=17.06.2~ce-0~ubuntu
-
 # Build script for Travis-CI.
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
